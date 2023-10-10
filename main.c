7bc982f40a68da3e8ae7b90d7ba7df059a5f1d29
Philipp Katzenberger <katzenberger.ph+github@gmail.com>
2023-10-10 20:47:51 +0200

    feat(main): interest calculation without for

--- main.c
+++ main.c
@@ -2,6 +2,38 @@
 
 int main()
 {
-    printf("Hello World!\n");
+    double capital = 100000.00;
+    double interest = 0.025;
+
+    printf("Startkapital: %.2lf Euro\n",capital);
+    printf("Effektiver Jahreszins: %.2lf %%\n\n", interest*100);
+
+    printf("---------------------\n");
+    printf("| Jahr |   Kapital  |\n");
+    printf("---------------------\n");
+    printf("|   0  |  %.2lf |\n", capital);
+    capital = capital + capital * interest;
+    printf("|   1  |  %.2lf |\n", capital);
+    capital = capital + capital * interest;
+    printf("|   2  |  %.2lf |\n", capital);
+    capital = capital + capital * interest;
+    printf("|   3  |  %.2lf |\n", capital);
+    capital = capital + capital * interest;
+    printf("|   4  |  %.2lf |\n", capital);
+    capital = capital + capital * interest;
+    printf("|   5  |  %.2lf |\n", capital);
+    capital = capital + capital * interest;
+    printf("|   6  |  %.2lf |\n", capital);
+    capital = capital + capital * interest;
+    printf("|   7  |  %.2lf |\n", capital);
+    capital = capital + capital * interest;
+    printf("|   8  |  %.2lf |\n", capital);
+    capital = capital + capital * interest;
+    printf("|   9  |  %.2lf |\n", capital);
+    capital = capital + capital * interest;
+    printf("|  10  |  %.2lf |\n", capital);
+    printf("---------------------\n");
+
+    printf("\n\n\n");
     return 0;
 }
