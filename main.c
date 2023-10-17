5d58abf56ac9ad80a10c630b7a3241309e5d2bf8
Philipp Katzenberger <katzenberger.ph+github@gmail.com>
2023-10-17 19:41:36 +0200

    chore(main): Improve dynamic formatting

--- main.c
+++ main.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
+#include <math.h>
 
 int main()
 {
     double capital = 100000.00;
     double interest = 0.025;
+    int years = 10;
 
     printf("Startkapital: %.2lf Euro\n",capital);
     printf("Effektiver Jahreszins: %.2lf %%\n\n", interest*100);
@@ -12,13 +14,17 @@ int main()
     printf("| Jahr |   Kapital  |\n");
     printf("---------------------\n");
 
-    for(int i = 0; i<11;i++){
-        if(i > 9){
-            printf("|  %d  |  %.2lf |\n",i, capital);
-        }
-        else {
+    for(int i = 0; i<years +1;i++){
+        int digits = floor(log10(abs(i))) + 1;
+
+        if(digits == 1){
             printf("|   %d  |  %.2lf |\n",i, capital);
+        } else if(digits == 2){
+            printf("|  %d  |  %.2lf |\n",i, capital);
+        } else if (digits == 3){
+            printf("| %d  |  %.2lf |\n",i, capital);
         }
+
 
         capital = capital + capital * interest;
     }
