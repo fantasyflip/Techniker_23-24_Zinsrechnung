22fae759dcd32cf8d3d34fb5ca3c8fce5aa9fe45
Philipp Katzenberger <katzenberger.ph+github@gmail.com>
2023-10-17 20:42:18 +0200

    chore(main): Use printf formatting

--- main.c
+++ main.c
@@ -3,37 +3,26 @@
 
 int main()
 {
-    double capital = 100000.00;
-    double interest = 0.025;
+    double capital = 100000.0;
+    double interest = 2.5;
     int years = 10;
 
     printf("Startkapital: %.2lf Euro\n",capital);
-    printf("Effektiver Jahreszins: %.2lf %%\n\n", interest*100);
+    printf("Effektiver Jahreszins: %.2lf %%\n\n", interest);
 
-    printf("---------------------\n");
-    printf("| Jahr |   Kapital  |\n");
-    printf("---------------------\n");
+    interest = interest / 100.0;
 
-    for(int i = 0; i<years +1;i++){
-        int digits = floor(log10(abs(i))) + 1;
+    printf("--------------------------\n");
+    printf("|    Jahr |    Kapital   |\n");
+    printf("--------------------------\n");
 
-        if(digits == 1){
-            printf("|   %d  |  %.2lf |\n",i, capital);
-        } else if(digits == 2){
-            printf("|  %d  |  %.2lf |\n",i, capital);
-        } else if (digits == 3){
-            printf("| %d  |  %.2lf |\n",i, capital);
-        }
-
+    for(int i = 0; i < years + 1; i++){
+        printf("|   %4d  |  %11.2lf |\n",i, capital);
 
         capital = capital + capital * interest;
     }
 
-    printf("---------------------\n");
-
-
-
-
+    printf("--------------------------\n");
 
     printf("\n\n\n");
     return 0;
